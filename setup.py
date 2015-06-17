4e95cb19a3530ad7a268a9b68f493663a8ba8b35
vangheem <vangheem@gmail.com>
2015-06-17 01:06:29 -0500

    Prepare collective.emailconfirmationregistration 1.0.0a1.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.1dev1'
+version = '1.0.0a1'
 
 setup(name='collective.emailconfirmationregistration',
       version=version,
